9e04aa971093dbe6f5c4e5c9587d2541fbeb00a7
zhuiks <zhuke@ukr.net>
2021-04-26 17:37:53 -0500

    refactor fullscreen toggle

--- app/frontend/webcomponents/ezra-fullscreen-toggle.js
+++ app/frontend/webcomponents/ezra-fullscreen-toggle.js
@@ -1,4 +1,6 @@
 import '@material/mwc-icon-button-toggle';
+import 'mousetrap';
+// import i18n from 'i18next';
 
 const template = document.createElement('template');
 template.innerHTML = `
@@ -13,10 +15,27 @@ class EzraFullscreenToggle extends HTMLElement {
   }
 
   connectedCallback() {
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.addEventListener('click', (e) => this.toggleFullScreen(e));
+    if (typeof platformHelper !== 'undefined' &&platformHelper.isMac()) {
+      return; // no fullscreen button for Mac
+    }
 
-    this.updateButton();
+    if (typeof platformHelper !== 'undefined' && (platformHelper.isWin() || platformHelper.isLinux()) && typeof Mousetrap !== 'undefined') { // Mousetrap should be globabl under window.Mousetrap
+      Mousetrap.bind('f11', () => {
+        this.toggleFullScreen();
+        this.updateIcon();
+      });
+    }
+
+    const platform = this.getPlatform();
+    if (platform) {
+      this.isFullscreen = platform.isFullScreen();
+    }
+
+    this.addEventListener('click', (e) => this.toggleFullScreen(e));
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.updateIcon();
+    this.updateTitle();
   }
 
   toggleFullScreen(e) {
@@ -28,26 +47,26 @@ class EzraFullscreenToggle extends HTMLElement {
 
     this.isFullscreen = !this.isFullscreen;
 
+    this.updateTitle();
+
     platform.toggleFullScreen();
   }
 
-  updateButton() {
-    const platform = this.getPlatform();
-    if (platform) {
-      this.isFullscreen = platform.isFullScreen();
-    }
-
+  updateIcon() {
     const button = this.shadowRoot.querySelector('#toggle-fullscreen');
     if (this.isFullscreen) {
       button.setAttribute('on', '');
     } else {
       button.removeAttribute('on');
     }
+  }
 
+  updateTitle() {
+    this.setAttribute('title', i18n.t(this.isFullscreen ? 'menu.exit-fullscreen' : 'menu.fullscreen')); // TODO: refactor global var
   }
 
   getPlatform() { 
-    // TODO: refactor global vars
+    // TODO: move to platformHelper && refactor global vars
     if (typeof platformHelper === 'undefined') {
       return null;
     }  
